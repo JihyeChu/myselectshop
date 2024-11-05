29cf0874707fc796c01869e952afe15f1e7e7a1c
ChuJH <09strawberry@naver.com>
2024-11-05 18:44:52 +0900

    불필요한 어노테이션 제거

--- src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
+++ src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
@@ -3,7 +3,6 @@ package com.sparta.myselectshop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MyselectshopApplicationTests {
 
     @Test
